622cdbf3db1be6fd57b8a11888dd8d9d1ec725ca
a-person-01001001 <51980001+a-person-01001001@users.noreply.github.com>
2019-06-19 13:35:28 -0500

    Update scrambler.cs
    Updated double move support, U2 U to U' (U2 U is the same as U'),  U U' removed. (they cancel each other out)

--- scrambler.cs
+++ scrambler.cs
@@ -1,6 +1,7 @@
 // a-person-01001001's original code
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 // A rubiks cube scramble generator
 
@@ -10,20 +11,22 @@ namespace Application
     {
 
         public static Dictionary<int, string> turnDictionary;
-
+        public static List<string> scramble;
         public static void Main()
         {
 
             Random r = new Random(1);
             int turnsChosen;
+            // a-person-01001001's original code
             int specialIndicator;
             try
             {
+                // a-person-01001001's original code
                 //adding a newline to make it look better
                 Console.WriteLine();
 
                 //to hold the scramble sequence
-                var scramble = new List<string> {};
+                scramble = new List<string> { };
 
 
                 //message to user
@@ -35,59 +38,96 @@ namespace Application
                 //depending on user input we will go through each one of them
                 for (int i = 0; i <= length; i++)
                 {
-                    // a-person-01001001's original code
-
                     //randomly chooose a turn
-                    turnsChosen = r.Next(1, 7); 
+                    turnsChosen = r.Next(1, 7);
 
                     //randomly choose if it is anti-clockwise (prime) or not, or a double move
-                    specialIndicator = r.Next(1, 4);
+                    specialIndicator = r.Next(1, 3);
 
                     //add the scramble
                     scramble.Add(Turns(turnsChosen, specialIndicator));
 
+                    //scramble last 2 values
+                    i = i - UpdateInsufficientTurn();
                 }
 
                 //output to user
                 scramble.ForEach(Console.Write);
+                // a-person-01001001's original code
                 Console.WriteLine(Environment.NewLine);
 
                 //give an option to do it again
-                Redo();
+              // Redo();
             }
-            
-            // a-person-01001001's original code (extreme protection measures)
 
-            catch (Exception)
+            catch (Exception e)
             {
-                Console.WriteLine($"Error.");
+                Console.WriteLine($"Error. {e.Message}");
             }
+        }
+        /// <summary>
+        /// Updates the insufficient turn.
+        /// </summary>
+        /// <returns>The insufficient turn.</returns>
+        private static int UpdateInsufficientTurn()
+        {
+            // a-person-01001001's original code
+            // a-person-01001001's original code
+
+            var item1 = scramble.Last<string>();
+            int indexOfLast = scramble.IndexOf(item1);
+            if(indexOfLast == 0) { return 0; }
+
+            var item2 = scramble[indexOfLast - 1].ToString();
+
+            if(item1.Contains("'"))
+            {
+                if(item2.TrimEnd()+"'" == item1.TrimEnd())
+                {
+                    //they are prime and non prime same turn
+                    scramble.RemoveAt(indexOfLast);
+                    scramble.RemoveAt(indexOfLast - 1);
+                    return 1;
+                }
+            }
+
+            if (item2.Contains("'"))
+            {
+                if (item1.TrimEnd() + "'" == item2.TrimEnd())
+                {
+                    //they are prime and non prime same trun
+                    scramble.RemoveAt(indexOfLast);
+                    scramble.RemoveAt(indexOfLast - 1);
+                    return 1;
+                }
+
+            }
+            if(item1.Contains("2") || item2.Contains("2")) { return 1; }
+            return 0;
+            // a-person-01001001's original code
         }
 
         /// <summary>
         /// Redo this instance.
         /// </summary>
-        // a-person-01001001's original code
-
         public static void Redo()
         {
             Console.WriteLine("Next scramble. Type 'Y' if you want it:\t");
             string nextScramble = Console.ReadLine();
             nextScramble = nextScramble.ToUpper();
-            if(nextScramble == "Y" || nextScramble == "YES")
+            if (nextScramble == "Y" || nextScramble == "YES")
             {
                 Main();
             }
-            else if(nextScramble == "N" || nextScramble == "NO")
+            else if (nextScramble == "N" || nextScramble == "NO")
             {
                 Environment.Exit(-1);
             }
-            else if(nextScramble != "N" && nextScramble != "NO" && nextScramble != "Y" && nextScramble != "YES")
+            else if (nextScramble != "N" && nextScramble != "NO" && nextScramble != "Y" && nextScramble != "YES")
             {
                 Console.WriteLine("Error.");
                 Redo();
                 // a-person-01001001's original code
-
             }
         }
 
@@ -100,32 +140,43 @@ namespace Application
         // my dad helped with this part. Hes much more advanced than me :P
         private static string Turns(int turn, int prime)
         {
+            // a-person-01001001's original code
+            var stringToSend = string.Empty;
             turnDictionary = new Dictionary<int, string>
             {
                 { 1, "F" },
                 { 2, "R" },
                 { 3, "U" },
                 { 4, "L" },
-                // a-person-01001001's original code
                 { 5, "D" },
                 { 6, "B" }
             };
 
             //if it is 2, it indicates it is prime
             if (prime == 2)
-            {
-                return turnDictionary[turn]+"' ";
-            }
-            // a-person-01001001's original code
-
-            else if (prime == 3)
-            {
                 // a-person-01001001's original code
-
-                return turnDictionary[turn] + "2 ";
+            {
+                stringToSend = turnDictionary[turn] + "' ";
             }
-            return turnDictionary[turn] + " ";
+            else
+            {
+                stringToSend = turnDictionary[turn] + " ";
+                if (scramble.Any())
+                    // a-person-01001001's original code
+                {
+                    if (scramble.Last<string>() == stringToSend)
+                    {
+                        //Console.Write("Match");
+                        scramble.Remove(scramble.Last<string>());
+                        scramble.Add(stringToSend.TrimEnd() + "2 ");
+                    }
+                    // a-person-01001001's original code
+                }
+            }
+
+            // a-person-01001001's original code
+            return stringToSend;
         }
     }
-} 
-// a-person-01001001's original code
+    // a-person-01001001's original code
+}
